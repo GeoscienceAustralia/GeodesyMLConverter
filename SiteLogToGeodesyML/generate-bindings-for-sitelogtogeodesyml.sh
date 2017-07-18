b7c9c7680199f7bdef2ce407330502b2da560b76
EC2 Default User <ec2-user@ip-172-31-11-60.ap-southeast-2.compute.internal>
2017-07-18 07:04:50 +0000

    Fixed output files names in binding generation script

--- SiteLogToGeodesyML/generate-bindings-for-sitelogtogeodesyml.sh
+++ SiteLogToGeodesyML/generate-bindings-for-sitelogtogeodesyml.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 set -e
-PYTHON_PREFIX=${HOME}/.local
-PYXBGEN=${PYTHON_PREFIX}/bin/pyxbgen
+PYTHON_PREFIX=.
+PYXBGEN=${PYTHON_PREFIX}/pyxbgen
 
-${PYXBGEN} -u ./modified-schemas/geodesyML.xsd -m eGeodesy --archive-path "${PYTHON_PREFIX}"/lib/python2.7/site-packages/pyxb/bundles/common/raw/:"${PYTHON_PREFIX}"/lib/python2.7/site-packages/pyxb/bundles/opengis/raw/:.:+
+${PYXBGEN} -u ../modified-schemas/geodesyML.xsd \
+	-m sitelogtogeodesyml \
+	--archive-path "${PYTHON_PREFIX}"/pyxb/bundles/common/raw/:"${PYTHON_PREFIX}"/pyxb/bundles/opengis/raw/:.:+
