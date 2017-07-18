afc3c87d2e2ab456be352523771e9c9f745171b3
EC2 Default User <ec2-user@ip-172-31-11-60.ap-southeast-2.compute.internal>
2017-07-18 01:06:12 +0000

    Deleted everything from GeodesyML subtree except for schemas

--- GeodesyML/codelists/validate-antenna-receiver-codelists.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-../tools/xml-schemer/bin/schemer.sh schema --xml antenna-receiver-codelists.xml --xsd ../schemas/third-party/iso-19139-20070417/gmx/gmx.xsd
--- GeodesyML/tools/xml-schemer/update-subtree-from-upstream.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-cd $(git rev-parse --show-toplevel)
-git subtree pull --prefix tools/xml-schemer https://github.com/GeoscienceAustralia/xml-schemer master --squash
