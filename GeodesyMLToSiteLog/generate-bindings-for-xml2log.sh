bd6b35a1df91c943455adde0eb7587257cc21f9a
EC2 Default User <ec2-user@ip-172-31-11-60.ap-southeast-2.compute.internal>
2017-07-18 01:11:16 +0000

    Added modified-schemas

--- GeodesyMLToSiteLog/generate-bindings-for-xml2log.sh
+++ GeodesyMLToSiteLog/generate-bindings-for-xml2log.sh
@@ -5,4 +5,4 @@ PYTHON_PREFIX=${HOME}/.local
 PYXBGEN=${PYTHON_PREFIX}/bin/pyxbgen
 
 wget https://raw.githubusercontent.com/GeoscienceAustralia/GeodesyML/master/schemas/geodesyML.xsd
-${PYXBGEN} -u geodesyML.xsd -m xml2log_bindings --archive-path "${PYTHON_PREFIX}"/lib/python2.7/site-packages/pyxb/bundles/common/raw/:"${PYTHON_PREFIX}"/lib/python2.7/site-packages/pyxb/bundles/opengis/raw/:.:+
+${PYXBGEN} -u ../GeodesyML/schemas/geodesyML.xsd -m xml2log_bindings --archive-path "${PYTHON_PREFIX}"/lib/python2.7/site-packages/pyxb/bundles/common/raw/:"${PYTHON_PREFIX}"/lib/python2.7/site-packages/pyxb/bundles/opengis/raw/:.:+
--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # GMLConverter
 Tools for converting between Geodesy XML Site Log and Text Site Log formats
 
-Bindings generated from https://github.com/GeoscienceAustralia/GeodesyML/commits/master commit-id 14368d7
-
 ## Deployment
 
 ```bash
