7d3e7a303469983855eb0d4b975df2bb0283467c
EC2 Default User <ec2-user@ip-172-31-11-60.ap-southeast-2.compute.internal>
2017-07-24 06:48:28 +0000

    Removed some unnecessary code and changed logging level to info

--- GeodesyMLToSiteLog/geodesymltositelog_lambda.py
+++ GeodesyMLToSiteLog/geodesymltositelog_lambda.py
@@ -6,10 +6,11 @@ import boto3
 
 import geodesymltositelog
 
-logging.basicConfig(level=logging.INFO)
-
 def lambda_handler(event, context):
     logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+
+    output_bucket_name, gws_url = os.environ['output_bucket_name'], os.environ['gws_url']
 
     try:
         sns_message = json.loads(event['Records'][0]['Sns']['Message'])
@@ -28,10 +29,6 @@ def lambda_handler(event, context):
         request = urllib2.Request(request_url, headers={'Accept': 'application/xml'})
         xml_string = urllib2.urlopen(request).read()
 
-    except KeyError:
-        logger.error('Output bucket name not given as Lambda environment variable')
-        raise 
-
     except:
         logger.error('Failed to get GeodesyML document for {} from GWS API'.format(
             sns_message['fourCharacterId']))
@@ -40,23 +37,18 @@ def lambda_handler(event, context):
     try:
         site_log_filename, site_log_data = geodesymltositelog.parseXML(xml_string)
 
-    except:
+    except Exception as err:
         # There is currently no error handling in the xml2log module, so expect any exception
-        logger.error('Failed to parse XML to site log for {}:\n{}'.format(
-            sns_message['fourCharacterId'], xml_string))
+        logger.error('Failed to parse XML to site log for {}:\n{}\n{}\n{}'.format(
+            sns_message['fourCharacterId'], str(err), repr(err), xml_string))
         raise
 
     try:
-        output_bucket_name = os.environ['output_bucket_name']
         boto3.client('s3').put_object(
             Bucket=output_bucket_name, Key=site_log_filename, Body=site_log_data)
 
         logger.info('Site log XML for {} parsed to s3://{}/{}'.format(
             sns_message['fourCharacterId'], output_bucket_name, site_log_filename))
-
-    except KeyError:
-        logger.error('Output bucket name not given as Lambda environment variable')
-        raise
 
     except:
         logger.error('Failed to output Site Log data to s3://{}/{}'.format(
