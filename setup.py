99dd42277490277ad6ce6979bad639e17872486a
lbodor <lazar.bodor@ga.gov.au>
2017-08-11 10:03:59 +1000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,35 @@
+"Setup.py for GeodesyML Converter"
+
+#pylint: disable=line-too-long
+
+from distutils.command.build import build
+from subprocess import check_call
+from setuptools import setup
+
+class GenerateBindings(build):
+    "Generate GeodesyML bindings."
+
+    def run(self):
+        check_call(['cd GeodesyMLToSiteLog && ./generate-bindings-for-geodesymltositelog.sh'], shell=True)
+        check_call(['cd SiteLogToGeodesyML && ./generate-bindings-for-sitelogtogeodesyml.sh'], shell=True)
+        build.run(self)
+
+setup(
+    name='geodesyml_converter',
+    version='0.1',
+    license='Creative Commons 4.0',
+    packages=['GeodesyMLToSiteLog', 'SiteLogToGeodesyML'],
+    cmdclass={
+        'build': GenerateBindings,
+    },
+    entry_points={
+        'console_scripts': [
+            'geodesyml-to-sitelog=GeodesyMLToSiteLog.geodesymltositelog:main',
+            'sitelog-to-geodesyml=SiteLogToGeodesyML.sitelogtogeodesyml:main',
+            ],
+    },
+    install_requires=[
+        'iso3166',
+        'pyxb',
+    ],
+)
