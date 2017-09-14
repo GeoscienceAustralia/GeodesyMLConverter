9bf0c82284e49d37a58c4ed6716f98799a935d62
lbodor <lazar.bodor@ga.gov.au>
2017-09-14 19:26:41 +1000

    Bump terraform

--- travis/install-terraform.sh
+++ travis/install-terraform.sh
@@ -5,7 +5,7 @@ set -e
 cd "$HOME"
 
 if [ -z "$(ls -A terraform)" ]; then
-    curl -fSL "https://releases.hashicorp.com/terraform/0.6.15/terraform_0.6.15_linux_amd64.zip" -o terraform.zip
+    curl -fSL "https://releases.hashicorp.com/terraform/0.10.4/terraform_0.10.4_linux_amd64.zip" -o terraform.zip
     unzip terraform.zip -d terraform
     rm -f terraform.zip
 fi
