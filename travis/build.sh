d45d1663bb48e53c3283cfd13e478ba861a8a9db
lbodor <lazar.bodor@ga.gov.au>
2017-09-14 19:52:32 +1000

    CI, configure aws cli in the default profile

--- travis/build.sh
+++ travis/build.sh
@@ -7,9 +7,9 @@ pytest
 cd aws/
 
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-	aws configure set aws_access_key_id "${TRAVIS_AWS_ACCESS_KEY_ID}" --profile geodesy
-	aws configure set aws_secret_access_key "${TRAVIS_AWS_SECRET_KEY_ID}" --profile geodesy
-	aws configure set region ap-southeast-2 --profile geodesy
+	aws configure set aws_access_key_id "${TRAVIS_AWS_ACCESS_KEY_ID}"
+	aws configure set aws_secret_access_key "${TRAVIS_AWS_SECRET_KEY_ID}"
+	aws configure set region ap-southeast-2
 	case "${TRAVIS_BRANCH}" in
 		"next")
 			./deploy.sh dev auscors-terraform-state-dev
