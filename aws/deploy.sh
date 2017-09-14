f9789b45be1b61a9d3a6755c41c491cb7535bddc
lbodor <lazar.bodor@ga.gov.au>
2017-09-14 17:10:57 +1000

    Remove tabs

--- aws/deploy.sh
+++ aws/deploy.sh
@@ -8,10 +8,10 @@ cd auscors-sitelogs-terraform/
 ./modules/lambda/create-deployment-package.sh
 
 terraform init \
-	-backend-config "bucket=${terraform_state}" \
-	-backend-config "lock_table=${terraform_state}" \
-	-backend-config "region=ap-southeast-2" \
-	-backend-config "key=auscors-sitelogs/${environment}/terraform.tfstate"
+    -backend-config "bucket=${terraform_state}" \
+    -backend-config "lock_table=${terraform_state}" \
+    -backend-config "region=ap-southeast-2" \
+    -backend-config "key=auscors-sitelogs/${environment}/terraform.tfstate"
 
 terraform get
 terraform plan -var-file=$environment.tfvars
