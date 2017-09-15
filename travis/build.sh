13cca6a66e225f9943578c006d954a517b3045a5
EC2 Default User <ec2-user@ip-172-31-11-60.ap-southeast-2.compute.internal>
2017-09-15 00:40:24 +0000

    Add prod deployment user access keys, add deploy to prod environment from prod git branch

--- travis/build.sh
+++ travis/build.sh
@@ -7,15 +7,24 @@ pytest
 cd aws/
 
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-	aws configure set aws_access_key_id "${TRAVIS_AWS_ACCESS_KEY_ID}"
-	aws configure set aws_secret_access_key "${TRAVIS_AWS_SECRET_KEY_ID}"
-	aws configure set region ap-southeast-2
 	case "${TRAVIS_BRANCH}" in
 		"next")
+			aws configure set aws_access_key_id "${TRAVIS_AWS_ACCESS_KEY_ID}"
+			aws configure set aws_secret_access_key "${TRAVIS_AWS_SECRET_KEY_ID}"
+			aws configure set region ap-southeast-2
 			./deploy.sh dev auscors-terraform-state-dev
 		;;
 		"master")
+			aws configure set aws_access_key_id "${TRAVIS_AWS_ACCESS_KEY_ID}"
+			aws configure set aws_secret_access_key "${TRAVIS_AWS_SECRET_KEY_ID}"
+			aws configure set region ap-southeast-2
 			./deploy.sh test auscors-terraform-state-dev
+		;;
+		"prod")
+			aws configure set aws_access_key_id "${TRAVIS_AWS_ACCESS_KEY_ID_PROD}"
+			aws configure set aws_secret_access_key "${TRAVIS_AWS_SECRET_KEY_ID_PROD}"
+			aws configure set region ap-southeast-2
+			./deploy.sh prod auscors-terraform-state-prod
 		;;
 	esac
 fi
