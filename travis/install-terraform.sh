c1718f7b9d8634fbdc5ba3ede98f963801c76282
lbodor <lazar.bodor@ga.gov.au>
2018-01-18 09:28:02 +1100

    Terraform: 0.10.5 -> 0.10.8

--- travis/install-terraform.sh
+++ travis/install-terraform.sh
@@ -5,7 +5,7 @@ set -e
 cd "$HOME"
 
 if [ -z "$(ls -A terraform)" ]; then
-    curl -fSL "https://releases.hashicorp.com/terraform/0.10.5/terraform_0.10.5_linux_amd64.zip" -o terraform.zip
+    curl -fSL "https://releases.hashicorp.com/terraform/0.10.8/terraform_0.10.8_linux_amd64.zip" -o terraform.zip
     unzip terraform.zip -d terraform
     rm -f terraform.zip
 fi
